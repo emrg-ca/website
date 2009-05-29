170f2771bb9f6a9dbea6d600ffcf6e285e85368b
Tyler Tidman <tyler.tidman@draak.ca>
2009-05-29 16:41:28 -0400

    Initial commit for 20081213

--- shared/corefunctions.js
+++ shared/corefunctions.js
@@ -159,11 +159,11 @@ function header_fr() {
 
 function menu_en() {
 	document.write('       	<div id="linkspace">');
-	document.write('            <a class="menubar" target="_top" href="' + translated_href + '">Français</a> | ');
+	document.write('            <a class="menubar" target="_top" href="' + translated_href + '">Fran&ccedil;ais</a> | ');
 	document.write('            <a class="menubar" href="index.htm">Home</a> | ');
 	document.write('            <a class="menubar" href="contactus.htm">Contact Us</a> | ');
 	document.write('            <a class="menubar" href="services.htm">Services</a> | ');
-	document.write('            <a class="menubar" href="meetings.htm">Meetings</a> | ');
+	document.write('            <a class="menubar" href="meeting_general.htm">Meetings</a> | ');
 	document.write('            <a class="menubar" href="external_url.htm?http://www.weatheroffice.ec.gc.ca/city/pages/on-118_metric_e.html">Weather Forecast</a> | ');
 	document.write('            <a class="menubar" href="library.htm">Library</a> | ');
 	document.write('            <a class="menubar" href="search.htm">Search</a>');
@@ -176,7 +176,7 @@ function menu_fr() {
 	document.write('            <a class="menubar" target="_top" href="http://translate.google.com/translate?u=http%3A%2F%2Fwww.emrg.ca%2Findex.htm&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools">&Agrave; la maison</A> | ');
 	document.write('            <a class="menubar" target="_top" href="http://translate.google.com/translate?u=http%3A%2F%2Fwww.emrg.ca%2Fcontactus.htm&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools">Contactez-Nous</A> | ');
 	document.write('            <a class="menubar" target="_top" href="http://translate.google.com/translate?u=http%3A%2F%2Fwww.emrg.ca%2Fservices.htm&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools">Services</A> | ');
-	document.write('            <a class="menubar" target="_top" href="http://translate.google.com/translate?u=http%3A%2F%2Fwww.emrg.ca%2Fmeetings.htm&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools">R&eacute;unions</A> | ');
+	document.write('            <a class="menubar" target="_top" href="http://translate.google.com/translate?u=http%3A%2F%2Fwww.emrg.ca%2Fmeeting_general.htm&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools">R&eacute;unions</A> | ');
 	document.write('            <a class="menubar" href="external_url.htm?http://www.weatheroffice.ec.gc.ca/city/pages/on-118_metric_f.html">pr&eacute;visions m&eacute;t&eacute;orologiques</a> | ');
 	document.write('            <a class="menubar" target="_top" href="http://translate.google.com/translate?u=http%3A%2F%2Fwww.emrg.ca%2Flibrary.htm&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools">Biblioth&egrave;que</A> | ');
 	document.write('            <a class="menubar" target="_top" href="http://translate.google.com/translate?u=http%3A%2F%2Fwww.emrg.ca%2Fsearch.htm&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools">Recherche</A>');
@@ -184,90 +184,93 @@ function menu_fr() {
 }
 
 function en2frURL(URL_en) {
-    encoded_URL1 = URL_en.replace(/:/g, "%3A");
-    encoded_URL2 = encoded_URL1.replace(/\//g, "%2F");
-    URL_fr = "http://translate.google.com/translate?u=" + encoded_URL2 + "&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools";
-    return(URL_fr);
+	encoded_URL1 = URL_en.replace(/:/g, "%3A");
+	encoded_URL2 = encoded_URL1.replace(/\//g, "%2F");
+	URL_fr = "http://translate.google.com/translate?u=" + encoded_URL2 + "&langpair=en%7Cfr&hl=en&ie=UTF-8&oe=UTF-8&prev=%2Flanguage_tools";
+	return(URL_fr);
 }
 
 function fr2enURL(URL_fr) {
-    form_info=createRequestObject(URL_fr);
-    var URL_en=form_info['u'];
-    return(URL_en);
+	form_info=createRequestObject(URL_fr);
+	var URL_en=form_info['u'];
+	return(URL_en);
 }
 
 function createRequestObject(passed_href) {
 
-  TEMP_FORM_DATA = new Object();
-    // The Object ("Array") where our data will be stored.
+	TEMP_FORM_DATA = new Object();
+	// The Object ("Array") where our data will be stored.
 
-  separator = ',';
-    // The token used to separate data from multi-select inputs
+	separator = ',';
+	// The token used to separate data from multi-select inputs
 
-  query = '' + passed_href;
+	query = '' + passed_href;
 
-  qu = query
-    // Get the current URL so we can parse out the data.
-    // Adding a null-string '' forces an implicit type cast
-    // from property to string, for NS2 compatibility.
+	qu = query
 
-  query = query.substring((query.indexOf('?')) + 1);
-    // Keep everything after the question mark '?'.
+	// Get the current URL so we can parse out the data.
+	// Adding a null-string '' forces an implicit type cast
+	// from property to string, for NS2 compatibility.
 
-  if (query.length < 1) { return false; }  // Perhaps we got some bad data?
+	query = query.substring((query.indexOf('?')) + 1);
+	// Keep everything after the question mark '?'.
 
-  keypairs = new Object();
-  numKP = 1;
-    // Local vars used to store and keep track of name/value pairs
-    // as we parse them back into a usable form.
+	if (query.length < 1) { return false; }  // Perhaps we got some bad data?
 
-  while (query.indexOf('&') > -1) {
-    keypairs[numKP] = query.substring(0,query.indexOf('&'));
-    query = query.substring((query.indexOf('&')) + 1);
-    numKP++;
-      // Split the query string at each '&', storing the left-hand side
-      // of the split in a new keypairs[] holder, and chopping the query
-      // so that it gets the value of the right-hand string.
-  }
+	keypairs = new Object();
+	numKP = 1;
+	// Local vars used to store and keep track of name/value pairs
+	// as we parse them back into a usable form.
 
-  keypairs[numKP] = query;
-    // Store what's left in the query string as the final keypairs[] data.<
+	while (query.indexOf('&') > -1) {
+		keypairs[numKP] = query.substring(0,query.indexOf('&'));
+		query = query.substring((query.indexOf('&')) + 1);
+		numKP++;
+		// Split the query string at each '&', storing the left-hand side
+		// of the split in a new keypairs[] holder, and chopping the query
+		// so that it gets the value of the right-hand string.
+	}
 
-  for (i in keypairs) {
-    keyName = keypairs[i].substring(0,keypairs[i].indexOf('='));
-      // Left of '=' is name.
-    keyValue = keypairs[i].substring((keypairs[i].indexOf('=')) + 1);
-      // Right of '=' is value.
-    while (keyValue.indexOf('+') > -1) {
-      keyValue = keyValue.substring(0,keyValue.indexOf('+')) + ' ' + keyValue.substring(keyValue.indexOf('+') + 1);
-        // Replace each '+' in data string with a space.
-    }
+	keypairs[numKP] = query;
+	// Store what's left in the query string as the final keypairs[] data.<
 
-    keyValue = unescape(keyValue);
-      // Unescape non-alphanumerics
+	for (i in keypairs) {
+		keyName = keypairs[i].substring(0,keypairs[i].indexOf('='));
 
-    if (TEMP_FORM_DATA[keyName]) {
-      TEMP_FORM_DATA[keyName] = TEMP_FORM_DATA[keyName] + separator + keyValue;
-        // Object already exists, it is probably a multi-select input,
-        // and we need to generate a separator-delimited string
-        // by appending to what we already have stored.
-    } else {
-      TEMP_FORM_DATA[keyName] = keyValue;
-        // Normal case: name gets value.
-    }
+		// Left of '=' is name.
+		keyValue = keypairs[i].substring((keypairs[i].indexOf('=')) + 1);
 
-  }
-  return(TEMP_FORM_DATA);
+		// Right of '=' is value.
+		while (keyValue.indexOf('+') > -1) {
+			keyValue = keyValue.substring(0,keyValue.indexOf('+')) + ' ' + keyValue.substring(keyValue.indexOf('+') + 1);
+			// Replace each '+' in data string with a space.
+		}
+
+		keyValue = unescape(keyValue);
+		// Unescape non-alphanumerics
+
+		if (TEMP_FORM_DATA[keyName]) {
+			TEMP_FORM_DATA[keyName] = TEMP_FORM_DATA[keyName] + separator + keyValue;
+			// Object already exists, it is probably a multi-select input,
+			// and we need to generate a separator-delimited string
+			// by appending to what we already have stored.
+		} else {
+			TEMP_FORM_DATA[keyName] = keyValue;
+			// Normal case: name gets value.
+		}
+
+	}
+
+	return(TEMP_FORM_DATA);
 }
 
 function leavingEMRG() {
-    if  (displayLanguage=="en") {
-        alert("You are now leaving the Emergency Measures Radio Group's website. This link is provided solely for your convenience. The the Emergency Measures Radio Group is not responsible for the information found through it.");
-    }
-    else {
-        alert("Vous laissez maintenant le website du groupe par radio de mesures de secours.  Ce lien est donn&eacute; seulement pour votre convenance.  Le le groupe par radio de mesures de secours n'est pas responsable de l'information trouv&eacute;e par elle.");
-    };
-
+	if  (displayLanguage=="en") {
+		alert("You are now leaving the Emergency Measures Radio Group's website. This link is provided solely for your convenience. The the Emergency Measures Radio Group is not responsible for the information found through it.");
+	}
+	else {
+		alert("Vous laissez maintenant le website du groupe par radio de mesures de secours.  Ce lien est donn&eacute; seulement pour votre convenance.  Le le groupe par radio de mesures de secours n'est pas responsable de l'information trouv&eacute;e par elle.");
+	};
 }
 
 function hello_world() {
@@ -309,4 +312,33 @@ function spam_vaccine( thehrefArray, theTextArray, theQBulletPath) {
 	}
 	document.write(theText);
 }
+
+function setEventReminder(p_YYYY, p_MM, p_DD, p_HH, p_SS)
+{
+	//Set the two dates
+	today=new Date()
+
+	// format of Date is (YYYY, MM, DD, HH, MM) where Month is 0-11 in JavaScript
+	eventDate=new Date(p_YYYY, p_MM-1, p_DD, p_HH, p_SS) 
+
+	//Calculate 1 day in milliseconds
+	var one_day_in_ms=1000*60*60*24
+
+	//Calculate difference btw the two dates (in milliseconds), and convert to days
+	numberOfDaysToEvent=Math.ceil((eventDate.getTime()-today.getTime())/(one_day_in_ms));
+
+	if (numberOfDaysToEvent >= 0) {
+		if  (numberOfDaysToEvent == 0) {
+			eventDate="TODAY"
+		}
+
+		if (numberOfDaysToEvent==1) {
+			eventDate="TOMORROW"
+		}
+	}
+	else {
+		eventDate=""
+	}
+}
+
 -->
