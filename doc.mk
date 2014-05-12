d8390785d4aa9def4e4a854145af03ad702aa775
Tyler Tidman <tyler.tidman@draak.ca>
2014-05-12 18:35:50 -0400

    Ensure we can run doc conversion scripts even when a copy of LibreOffice is already running

--- doc.mk
+++ doc.mk
@@ -4,29 +4,37 @@ SHELL := /bin/bash
 all :
 
 %.pdf : %.odt
-	@libreoffice --headless --convert-to pdf $^
+	@libreoffice --headless --convert-to pdf $^ \
+  "-env:UserInstallation=file:///tmp/libreofficebug"
 
 %.pdf : %.odp
-	@libreoffice --headless --convert-to pdf $^
+	@libreoffice --headless --convert-to pdf $^ \
+  "-env:UserInstallation=file:///tmp/libreofficebug"
 
 .PRECIOUS : %.odt
 %.odt : %.doc
-	@libreoffice --headless --convert-to odt $^
+	@libreoffice --headless --convert-to odt $^ \
+  "-env:UserInstallation=file:///tmp/libreofficebug"
 
 %.odt : %.docx
-	@libreoffice --headless --convert-to odt $^
+	@libreoffice --headless --convert-to odt $^ \
+  "-env:UserInstallation=file:///tmp/libreofficebug"
 
 .PRECIOUS : %.odp
 %.odp : %.ppt
-	@libreoffice --headless --convert-to odp $^
+	@libreoffice --headless --convert-to odp $^ \
+  "-env:UserInstallation=file:///tmp/libreofficebug"
 
 %.odp : %.pptx
-	@libreoffice --headless --convert-to odp $^
+	@libreoffice --headless --convert-to odp $^ \
+  "-env:UserInstallation=file:///tmp/libreofficebug"
 
 # Word Template from OpenDocument Text Template
 #%.dot : %.otd
-#	@libreoffice --headless --convert-to dot $^
+#	@libreoffice --headless --convert-to dot $^ \
+# "-env:UserInstallation=file:///tmp/libreofficebug"
 
 # Powerpoint Template from OpenDocument Presentation Template
 #%.pot : %.otp
-#	@libreoffice --headless --convert-to pot $^
+#	@libreoffice --headless --convert-to pot $^ \
+# "-env:UserInstallation=file:///tmp/libreofficebug"
